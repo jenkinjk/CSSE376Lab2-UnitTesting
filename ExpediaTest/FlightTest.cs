d7636015937bdb1bdd610358b5b7725085577ad3
jenkinjk <jenkinjk@rose-hulman.edu>
2015-03-19 15:07:42 -0400

    Completed Lab
    Done

--- ExpediaTest/FlightTest.cs
+++ ExpediaTest/FlightTest.cs
@@ -7,6 +7,44 @@ namespace ExpediaTest
 	[TestFixture()]
 	public class FlightTest
 	{
-		//TODO Task 7 items go here
+		private readonly DateTime leaves = new DateTime(2011,12,13);
+        private readonly DateTime returns = new DateTime(2011,12,17);
+        private readonly int distance = 1000;
+        [Test()]
+        public void TestThatFlightInitializes()
+        {
+            var target = new Flight(leaves, returns, distance);
+            Assert.IsNotNull(target);
+        }
+        [Test()]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void TestThatPlaneThrowsOnBadFlightLength()
+        {
+            new Flight(leaves, returns, -5);
+        }
+        [Test()]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void TestThatPlaneThrowsOnBadFlightDays()
+        {
+            new Flight(new DateTime(2012,12,13),returns,distance);
+        }
+        [Test()]
+        public void TestThatFlightHasCorrectBasePriceForOneDayStay()
+        {
+            var target = new Flight(new DateTime(2012, 12, 13), new DateTime(2012, 12, 14), distance);
+            Assert.AreEqual(220, target.getBasePrice());
+        }
+        [Test()]
+        public void TestThatFlightHasCorrectBasePriceForTwoDayStay()
+        {
+            var target = new Flight(new DateTime(2012, 12, 13), new DateTime(2012, 12, 15), distance);
+            Assert.AreEqual(240, target.getBasePrice());
+        }
+        [Test()]
+        public void TestThatFlightHasCorrectBasePriceForTenDayStay()
+        {
+            var target = new Flight(new DateTime(2012, 12, 13), new DateTime(2012, 12, 23), distance);
+            Assert.AreEqual(400, target.getBasePrice());
+        }
 	}
 }
